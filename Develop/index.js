7e5c2119d3bec4ba112606b64cb74ef244cabb90
Drew Vena <drewvena1@gmail.com>
2021-02-04 13:16:17 -0600

    got it generating

--- Develop/index.js
+++ Develop/index.js
@@ -1,94 +1,95 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const fs = require('fs')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = ['What is your GitHub username?', 'What is your email address', "What is your project's name", 'Please write a short description of your project', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
-
-const promptUser = () => {
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'github',
-        message: questions[0] + '(Required)',
-        validate: githubInput => {
-        if (githubInput) {
-          return true;
+const questions = [{
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?(Required)',
+    validate: githubInput => {
+    if (githubInput) {
+      return true;
+    } else {
+      console.log('Please enter your GitHub username!');
+      return false;
+    }
+  }
+}, 
+{
+    type:'input',
+    name:'email',
+    message: 'What is your email address(Required)',
+    validate: emailInput => {
+        if (emailInput) {
+            return true;
         } else {
-          console.log('Please enter your GitHub username!');
-          return false;
+            console.log ('Please enter your E-mail!')
+            return false
         }
-      }
-    },
+    }
+},
+{
+    type:'input',
+    name:'title',
+    message: "What is your project's name"
 
-    {
-        type:'input',
-        name:'email',
-        message: questions[1] + '(Required)',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log ('Please enter your E-mail!')
-                return false
-            }
-        }
-    },
-    {
-        type:'input',
-        name:'project name',
-        message: questions[2]
+},  
+{
+    type:'input',
+    name:'description',
+    message:'Please write a short description of your project'
+},  
+{
+    type:'list',
+    name:'licenses',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+    
+},  
+{
+    type:'input',
+    name:'dependency',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i'
+},   
+{
+    type:'input',
+    name:'tests',
+    message: 'What command should be run to run tests?' ,
+    default: 'npm test'
+},   
+{
+    type:'input',
+    name:'know',
+    message: 'What does the user need to know about using the repo?',
+} ,   
+ {
+    type:'input',
+    name:'about',
+    message:'What does the user need to know about contributing to the repo?' 
+},];
 
-    },
-    {
-        type:'input',
-        name:'description',
-        message: questions[3]
-    },
-    {
-        type:'list',
-        name:'licenses',
-        message: questions[4],
-        choices: ['MIT', 'Apache', 'GPL', 'BSD'],
-        
-    },
-    {
-        type:'input',
-        name:'depedency',
-        message: questions[5],
-        default: 'npm i'
-    },
-    {
-        type:'input',
-        name:'tests',
-        message: questions[6],
-        default: 'npm test'
-    },
-    {
-        type:'input',
-        name:'know',
-        message: questions[7],
-    },
-    {
-        type:'input',
-        name:'about',
-        message:questions[8],
-    },
-    {
-        type:'input',
-        name:'contributing',
-        message: questions[9]
-    }   
-])
-}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('README.md')
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, err =>{
+        if (err){
+            return console.log (err);
+
+        }else{
+            console.log('Your README.md has been created!')
+        }
+    })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser();
+    inquirer.prompt(questions)
+    .then(data => {
+    writeToFile('README.md', generateMarkdown(data))
+    })
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if ('MIT') ![License]('https://img.shields.io/badge/license-MIT-blue.svg');
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return ;
+  } else if (license === 'Apache'){
+    return;
+  } else if (license === 'GPL'){
+    return;
+  }else if (license === 'BSD'){
+    return;
+  }
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,7 +27,37 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+
+ ##  Description
+ ${data.description}
+ ---
+ ## Table of Contents
+ - [Installation](#installation)
+ -[Usage](#usage)
+ -[License](#license)
+ -[Contributing](#contributing)
+ -[Tests](#tests)
+ -[Questions](#questions)
+ ---
+ ## Installation
+ ${data.dependency}
+ --
+ ## Usage
+ ${data.know}
+ ## License
+ This project is licensed under the terms of ${data.licenses}
+ license.
+ --
+ ## Contributing
+ Run ${data.about}
+ --
+ ## Tests
+ Run ${data.tests} to run tests.
+ --
+ ## Questions
+ If you have any questions about this repo, contact ${data.github}/${data.title}
+
+`}
+renderLicenseLink();
 
 module.exports = generateMarkdown;
